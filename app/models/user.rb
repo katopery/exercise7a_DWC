25aaa5240eb858b7a692003a06a061b1fd6c7bb3
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-02-03 15:35:14 +0000

    [Add] 非同期通信前　前章解答合わせ

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,17 +41,17 @@ class User < ApplicationRecord
   end
   
   # 検索方法分岐
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?", "#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?", "%#{word}%")
+  def self.search_for(content, method)
+    if method == "perfect"
+      User.where("name LIKE?", "#{content}")
+    elsif method == "forward"
+      User.where("name LIKE?", "#{content}%")
+    elsif method == "backward"
+      User.where("name LIKE?", "%#{content}")
+    elsif method == "partial"
+      User.where("name LIKE?", "%#{content}%")
     else
-      @user = User.all
+      User.all
     end
   end
 
